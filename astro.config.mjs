9aab107771eec5a6e19fdcce5927b6d117b769a0
Hrihorii Ilin <frikadellios@gmail.com>
2025-05-17 17:22:14 +0100

    add some things

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import react from '@astrojs/react';
+
+import sitemap from '@astrojs/sitemap';
+
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     }
-  })
+  }),
+
+  integrations: [react(), sitemap(), mdx()]
 });
